f5524d1aeaef5fd73e3056d71cdae0bd3ddf924b
Vladimir Stankovic <vladast.dev@gmail.com>
2014-05-15 10:04:26 +0200

    Added additional OnError interface with additional integer parameter

--- src/io/github/vladast/avrcommunicator/OnAvrRecorderEventListener.java
+++ src/io/github/vladast/avrcommunicator/OnAvrRecorderEventListener.java
@@ -1,10 +1,15 @@
 package io.github.vladast.avrcommunicator;
 
+import java.util.ArrayList;
+
 public interface OnAvrRecorderEventListener {
 	public void OnDeviceFound();
 	public void OnDeviceConnected();
 	public void OnDeviceSearching();
 	public void OnDeviceReInitiated();
-	public void OnRecordsRead();
+	public void OnRecordsRead(ArrayList<Reading> eventReadings);
 	public void OnError(AvrRecorderErrors avrRecorderErrors);
+	public void OnError(AvrRecorderErrors avrRecorderErrors, int data);
+	public void OnReadingStarted();
+	public void OnDebugMessage(String message);
 }
