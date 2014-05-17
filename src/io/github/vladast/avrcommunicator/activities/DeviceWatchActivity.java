d6aee40f9ec846d354833bc52f5fdc1f0694010a
Vladimir Stankovic <vladast.dev@gmail.com>
2014-05-17 22:21:16 +0200

    Implemented event handlers + logging

--- src/io/github/vladast/avrcommunicator/activities/DeviceWatchActivity.java
+++ src/io/github/vladast/avrcommunicator/activities/DeviceWatchActivity.java
@@ -12,6 +12,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +22,6 @@ public class DeviceWatchActivity extends Activity implements OnAvrRecorderEventL
 	
 	private TextView mTextViewStatus;
 	private TextView mTextViewData;
-	private boolean mRecordsRead;
 	
 	private Communicator mCommunicator;
 	
@@ -49,57 +49,90 @@ public class DeviceWatchActivity extends Activity implements OnAvrRecorderEventL
         mCommunicator.stopDeviceDetection();
     }
 
+	private String parseAvrRecorderErrorCode(AvrRecorderErrors avrRecorderErrors) {
+		switch (avrRecorderErrors) {
+		case ERR_HEADER:
+			Log.e(Communicator.TAG, "Encountered error while reading header data from device.");
+			break;
+		case ERR_STATE:
+			Log.e(Communicator.TAG, "Encountered error while reading state data from device.");
+			break;
+		case ERR_SESSION:
+			Log.e(Communicator.TAG, "Encountered error while reading session data from device.");
+			break;
+		case ERR_ERROR:
+			Log.e(Communicator.TAG, "Encountered error while reading error data from device.");
+			break;
+		case ERR_COUNT:
+			Log.e(Communicator.TAG, "Encountered error while reading count data from device.");
+			break;
+		case ERR_RECORD:
+			Log.e(Communicator.TAG, "Encountered error while reading record data from device.");
+			break;
+		case ERR_SWITCH:
+			Log.e(Communicator.TAG, "Invalid switch code detected.");
+			break;
+		default:
+			Log.e(Communicator.TAG, "Unknown error code received.");
+			break;
+		}
+		return null;
+	}
+
 	@Override
 	public void OnDeviceFound() {
-		// TODO Auto-generated method stub
-		
+		mTextViewStatus.setText("Device has been found.");
+		Toast.makeText(this, "OnDeviceFound...", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void OnDeviceConnected() {
-		// TODO Auto-generated method stub
-		
+		mTextViewStatus.setText("Device has been connected.");
+		Toast.makeText(this, "OnDeviceConnected...", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void OnDeviceSearching() {
-		// TODO Auto-generated method stub
-		
+		mTextViewStatus.setText("Searching for device...");
+		Toast.makeText(this, "OnDeviceSearching...", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void OnDeviceReInitiated() {
-		// TODO Auto-generated method stub
-		
+		mTextViewStatus.setText("Device has been re-initiated.");
+		Toast.makeText(this, "OnDeviceReInitiated...", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void OnRecordsRead(ArrayList<Reading> eventReadings) {
-		// TODO Auto-generated method stub
-		
+		Toast.makeText(this, "OnRecordsRead: " + eventReadings.size(), Toast.LENGTH_LONG).show();
+		mTextViewData.setText("");
+		for(int i = 0; i < eventReadings.size(); ++i) {
+			mTextViewData.append(String.format("%d\t%s\t%d\n", eventReadings.get(i).getEntry(), eventReadings.get(i).getCodeName(), eventReadings.get(i).getTimestamp()));
+		}
 	}
 
 	@Override
 	public void OnError(AvrRecorderErrors avrRecorderErrors) {
-		// TODO Auto-generated method stub
 		Toast.makeText(this, "onError1", Toast.LENGTH_LONG).show();
+		Log.e(Communicator.TAG, parseAvrRecorderErrorCode(avrRecorderErrors));
 	}
 
 	@Override
 	public void OnError(AvrRecorderErrors avrRecorderErrors, int data) {
-		// TODO Auto-generated method stub
 		Toast.makeText(this, "onError2", Toast.LENGTH_LONG).show();
+		Log.e(Communicator.TAG, parseAvrRecorderErrorCode(avrRecorderErrors) + String.format("[data: %d]", data));
 	}
 
 	@Override
 	public void OnReadingStarted() {
 		// TODO Auto-generated method stub
-		
+		Toast.makeText(this, "OnReadingStarted", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void OnDebugMessage(String message) {
-		// TODO Auto-generated method stub
-		
+		//Toast.makeText(this, "OnDebugMessage: " + message, Toast.LENGTH_LONG).show();
+		Log.d(Communicator.TAG, message);
 	}    
 }
