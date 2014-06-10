8bcb32be9aa28d18121f0d77036abe91576b7701
Vladimir Stankovic <vladast.dev@gmail.com>
2014-06-10 23:31:11 +0200

    Implemented registerCommunicatorLIstener method - each activity can be registered as an device event listener

--- src/io/github/vladast/avrcommunicator/EventRecorderApplication.java
+++ src/io/github/vladast/avrcommunicator/EventRecorderApplication.java
@@ -4,6 +4,8 @@
 package io.github.vladast.avrcommunicator;
 
 import android.app.Application;
+import android.content.Context;
+import android.hardware.usb.UsbManager;
 import android.util.Log;
 
 /**
@@ -13,7 +15,15 @@ import android.util.Log;
 public class EventRecorderApplication extends Application {
 	private final String TAG = EventRecorderApplication.class.getSimpleName();
 	
-	private static EventRecorderApplication singleton;
+	/**
+	 * Single instance of <code>EventRecorderApplication</code> object.
+	 */
+	private static EventRecorderApplication eventRecorderApplication;
+	
+	/**
+	 * Instance of <code>Communicator</code> class that will be focal point for communication with USB device
+	 */
+	private Communicator mCommunicator;
 	
 	/**
 	 * Obtains single instance of <code>EventRecorderApplication</code> 
@@ -21,7 +31,7 @@ public class EventRecorderApplication extends Application {
 	 */
 	public EventRecorderApplication getInstance() {
 		Log.d(TAG, "Method getInstance called.");
-		return singleton;
+		return eventRecorderApplication;
 	}
 	
 	/**
@@ -31,7 +41,15 @@ public class EventRecorderApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		Log.d(TAG, "Method onCreate called.");
-		singleton = this;
+		eventRecorderApplication = this;
+		mCommunicator = new Communicator((UsbManager)getSystemService(Context.USB_SERVICE));
 	}
 
+	/**
+	 * Called from Android activity when needed to capture events coming out from attached USB device.
+	 * @param onAvrRecorderEventListener Implemented interface from attached object/activity.
+	 */
+	public void registerCommunicatorListener(OnAvrRecorderEventListener onAvrRecorderEventListener) {
+		mCommunicator.registerListener(onAvrRecorderEventListener);
+	}
 }
