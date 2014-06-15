b56077176b7224419d6942bf78412125723fe83d
Vladimir Stankovic <vladast.dev@gmail.com>
2014-06-15 22:26:12 +0200

    Implemented OnSharedPreferenceChangeListener interface

--- src/io/github/vladast/avrcommunicator/EventRecorderApplication.java
+++ src/io/github/vladast/avrcommunicator/EventRecorderApplication.java
@@ -3,16 +3,42 @@
  */
 package io.github.vladast.avrcommunicator;
 
+import io.github.vladast.avrcommunicator.activities.EventRecorderSettingsActivity;
+import io.github.vladast.avrcommunicator.activities.HomeScreenActivity;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+import org.xmlpull.v1.XmlSerializer;
+
 import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.XmlResourceParser;
+import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.util.Xml;
 
 /**
  * @author vladimir.stankovic
  * Singleton class representing customized <code>Application</code> object.
  */
-public class EventRecorderApplication extends Application {
+public class EventRecorderApplication extends Application implements OnSharedPreferenceChangeListener  {
 	private final String TAG = EventRecorderApplication.class.getSimpleName();
 	
 	/**
@@ -24,6 +50,82 @@ public class EventRecorderApplication extends Application {
 	 * Instance of <code>Communicator</code> class that will be focal point for communication with USB device
 	 */
 	private Communicator mCommunicator;
+	
+	/**
+	 * Device monitoring flag defined via <code>EventRecorderSettingsActivity</code> instance.
+	 */
+	private boolean mMonitorDevices;
+	
+	/**
+	 * Monitoring interval (in seconds) defined via <code>EventRecorderSettingsActivity</code> instance.
+	 */
+	private int mMonitoringInterval;
+	
+	/**
+	 * Sleep prevention flag defined via <code>EventRecorderSettingsActivity</code> instance.
+	 */
+	private boolean mPreventSleep;
+	
+	/**
+	 * Getter for device monitoring flag.
+	 * @return Device monitoring flag read from <code>EventRecorderSettingsActivity</code> instance.
+	 */
+	public boolean getMonitorDevices() {
+		return mMonitorDevices;
+	}
+	
+	/**
+	 * Setter for device monitoring flag.
+	 * <b>NOTE:</b> Should be called whenever Preference change occurs for device monitoring flag.
+	 * @param monitorDevices Device monitoring flag read from <code>EventRecorderSettingsActivity</code> instance..
+	 * @hide
+	 */
+	public void setMonitorDevices(boolean monitorDevices) {
+		mMonitorDevices = monitorDevices;
+		if(mMonitorDevices == true) {
+			mCommunicator.startDeviceDetection();
+		} else {
+			mCommunicator.stopDeviceDetection();
+		}
+	}
+	
+	/**
+	 * Getter for monitoring interval.
+	 * @return Monitoring interval read from <code>EventRecorderSettingsActivity</code> instance.
+	 */
+	public int getMonitoringInterval() {
+		return mMonitoringInterval;
+	}
+	
+	/** 
+	 * Setter for monitoring interval.
+	 * <b>NOTE:</b> Should be called whenever Preference change occurs for monitoring interval.
+	 * @param monitoringInterval Monitoring interval (in seconds) read from <code>EventRecorderSettingsActivity</code> instance.
+	 * @hide
+	 */
+	public void setMonitoringInterval(int monitoringInterval) {
+		mMonitoringInterval = (monitoringInterval > 0) ? monitoringInterval : AvrRecorderConstants.DEFAULT_PREF_MONITORING_INTERVAL;
+		Log.d(TAG, "Monitoring interval set to " + mMonitoringInterval + " seconds.");
+		mCommunicator.setMonitoringInterval(mMonitoringInterval);
+	}
+	
+	/**
+	 * Getter for sleep prevention flag.
+	 * @return Sleep prevention flag read from <code>EventRecorderSettingsActivity</code> instance.
+	 */
+	public boolean getPreventSleep() {
+		return mPreventSleep;
+	}
+	
+	/**
+	 * Setter for sleep prevention flag.
+	 * <b>NOTE:</b> Should be called whenever Preference change occurs for monitoring interval.
+	 * @param preventSleep
+	 */
+	public void setPreventSleep(boolean preventSleep) {
+		mPreventSleep = preventSleep;
+	}
+	
 	
 	/**
 	 * Obtains single instance of <code>EventRecorderApplication</code> 
@@ -42,9 +144,23 @@ public class EventRecorderApplication extends Application {
 		super.onCreate();
 		Log.d(TAG, "Method onCreate called.");
 		eventRecorderApplication = this;
+		
+		Log.d(TAG, "Creating Communicator instance...");
 		mCommunicator = new Communicator((UsbManager)getSystemService(Context.USB_SERVICE));
+		
+		PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);;
+		
+		mCommunicator.startDeviceDetection();
 	}
-
+	
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		Log.w(TAG, "System memory is low!");
+		Log.d(TAG, "Stopping device monitoring...");
+		mCommunicator.stopDeviceDetection();
+	}
+	
 	/**
 	 * Called from Android activity when needed to capture events coming out from attached USB device.
 	 * <b>NOTE:</b> Same as calling <code>registerListener</code> method from <code>Communicator</code> instance.
@@ -60,5 +176,24 @@ public class EventRecorderApplication extends Application {
 	 */
 	public Communicator getCommunicator() {
 		return mCommunicator;
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if (key.equals(EventRecorderSettingsActivity.KEY_PREF_MONITOR_DEVICES)) {
+        	mMonitorDevices = sharedPreferences.getBoolean(EventRecorderSettingsActivity.KEY_PREF_MONITOR_DEVICES, true);
+        	if(mMonitorDevices) {
+        		mCommunicator.startDeviceDetection();
+        	} else {
+        		mCommunicator.stopDeviceDetection();
+        	}
+        } else if (key.equals(EventRecorderSettingsActivity.KEY_PREF_MONITOR_INTERVAL)) {
+        	mMonitoringInterval = sharedPreferences.getInt(EventRecorderSettingsActivity.KEY_PREF_MONITOR_INTERVAL, 
+        			AvrRecorderConstants.DEFAULT_PREF_MONITORING_INTERVAL);
+        	mCommunicator.setMonitoringInterval(mMonitoringInterval);
+        } else if (key.equals(EventRecorderSettingsActivity.KEY_PREF_PREVENT_SLEEP)) {
+            mPreventSleep = sharedPreferences.getBoolean(EventRecorderSettingsActivity.KEY_PREF_PREVENT_SLEEP, true);
+            // TODO: Implement sleep prevention
+        }
 	}
 }
