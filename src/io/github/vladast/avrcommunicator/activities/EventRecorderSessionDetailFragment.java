17ad5f0768b6fe4e95bc056eaeb2337584943ec1
Vladimir Stankovic <vladast.dev@gmail.com>
2014-06-22 11:38:45 +0200

    Session information section draft completed - Edit/Save is yet to be implemented

--- src/io/github/vladast/avrcommunicator/activities/EventRecorderSessionDetailFragment.java
+++ src/io/github/vladast/avrcommunicator/activities/EventRecorderSessionDetailFragment.java
@@ -4,13 +4,17 @@ import java.sql.Date;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import io.github.vladast.avrcommunicator.EventRecorderApplication;
 import io.github.vladast.avrcommunicator.R;
-import io.github.vladast.avrcommunicator.R.id;
-import io.github.vladast.avrcommunicator.R.layout;
+import io.github.vladast.avrcommunicator.db.dao.DeviceDAO;
+import io.github.vladast.avrcommunicator.db.dao.EventDAO;
 import io.github.vladast.avrcommunicator.db.dao.SessionDAO;
 
 /**
@@ -90,10 +94,68 @@ public class EventRecorderSessionDetailFragment extends Fragment {
 				container, false);
 
 		// Show the dummy content as text in a TextView.
-		/*if (mItem != null) {
-			((TextView) rootView.findViewById(R.id.session_detail))
-					.setText(mItem.content);
-		}*/
+		if (mItem != null) {
+			// Set name and description directly from session DAO
+			((TextView) rootView.findViewById(R.id.textViewSessionName)).setText(mItem.getName());
+			((TextView) rootView.findViewById(R.id.textViewSessionDescription)).setText(mItem.getDescription());
+			
+			// Display message based on the number of events
+			String numberOfEvents = String.valueOf(mItem.getNumberOfEvents()) + " " + getResources().getString(R.string.home_recorded_events_only);
+			((TextView) rootView.findViewById(R.id.textViewSessionEventCount)).setText(numberOfEvents);
+			
+			// Display message based on recorded events duration
+			long duration = ((EventRecorderApplication)getActivity().getApplicationContext()).getDatabaseHandler().getDatabaseObjectValueCountByForeignId(EventDAO.class, EventDAO.DB_COUNTABLE_COLUMN, mItem);
+			String eventsRecorded = String.valueOf(duration) + " ";
+			
+			if(duration < 60) {
+				// seconds
+				eventsRecorded += getResources().getString(R.string.home_recorded_seconds);
+			} else if (duration >= 60 && duration < 120) {
+				// one minute
+				eventsRecorded += getResources().getString(R.string.home_recorded_minute);
+			} else if (duration >= 120 && duration < 3600) {
+				// minutes
+				eventsRecorded += getResources().getString(R.string.home_recorded_minutes);
+			} else if (duration >= 3600 && duration < 2 * 3600) {
+				// one hour
+				eventsRecorded += getResources().getString(R.string.home_recorded_hour);
+			} else {
+				// hours
+				eventsRecorded += getResources().getString(R.string.home_recorded_hours);
+			}		
+			
+			eventsRecorded += " " + getResources().getString(R.string.home_recorded_events);
+			((TextView) rootView.findViewById(R.id.textViewSessionEventsDuration)).setText(eventsRecorded);
+			
+			// Display date
+			((TextView) rootView.findViewById(R.id.textViewSessionRecordingDate)).setText(DateFormat.format("MM-DD-YYYY", mItem.getTimestampRecorded()));
+			
+			int deviceType = ((DeviceDAO)((EventRecorderApplication)getActivity().getApplicationContext()).getDatabaseHandler().getDatabaseObjectById(DeviceDAO.class, mItem.getIdDevice())).getType();
+			if(deviceType == DeviceDAO.DEVICE_TYPE_AVR) {
+				// TODO Display USB icon
+				((ImageView)rootView.findViewById(R.id.imageViewDevice)).setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher));
+			} else {
+				// TODO Display Android icon
+				((ImageView)rootView.findViewById(R.id.imageViewDevice)).setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher));
+			}
+			
+			// Set click listeners for Edit&Save buttons
+			rootView.findViewById(R.id.imageButtonSave).setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Save mItem details to database and display Toast of the final report
+				}
+			});
+			
+			rootView.findViewById(R.id.imageButtonEdit).setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Open Edit dialog fragment --> editing mItem object
+				}
+			});
+		}
 
 		return rootView;
 	}
