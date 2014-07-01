f00155da88153997ec78597fc2eb27d3a98fe2c5
Vladimir Stankovic <vladast.dev@gmail.com>
2014-07-01 22:27:13 +0200

    Placeholder for activity that is being started when no sessions are present in the database.

---
+++ src/io/github/vladast/avrcommunicator/activities/EventRecorderNoSessionsActivity.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package io.github.vladast.avrcommunicator.activities;
+
+import android.app.Activity;
+
+/**
+ * @author vladimir.stankovic
+ *
+ * Activity that is presented when no sessions are present in the database.
+ */
+public class EventRecorderNoSessionsActivity extends Activity {
+
+	/**
+	 * 
+	 */
+	public EventRecorderNoSessionsActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
